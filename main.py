9759a6ae6f9e1a150c561e4865b5c0fd71bbe901
vasiliy <vaszol@mail.ru>
2021-07-13 13:52:06 +0300

    Создание нового Приложения

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from kivymd.app import MDApp
+from kivymd.uix.label import MDLabel
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class MainApp(MDApp):
+    def build(self):
+        return MDLabel(text="Hello, Mortgage Calculator", halign="center")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+MortgageCalculatorApp().run()
